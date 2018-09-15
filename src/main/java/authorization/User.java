43181f1811221f3576ce16506f8b282b92152b94
Åìåëüÿí <drav10@rambler.ru>
2018-09-15 09:46:58 +0300

    Добавил Users

--- src/main/java/authorization/User.java
+++ src/main/java/authorization/User.java
@@ -1,13 +1,15 @@
 package authorization;
 
 public class User {
-    private String login = "";
+    private String login;
+private long currentMoney;
 
     public String getLogin() {
         return login;
     }
 
-    public User(String login) {
+    public User(String login, long cash) {
         this.login = login;
+        this.currentMoney = cash;
     }
 }
--- src/main/java/utils/Global.java
+++ src/main/java/utils/Global.java
@@ -1,5 +1,30 @@
 package utils;
 
+import authorization.User;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Global {
+    static {
+        accounts = getAllAccountsFromFile();
+    }
+
+    public static Map<String, User> accounts;
     public static final String session_attr_currentUser = "currentSessionUser";
+
+    private static Map<String, User> getAllAccountsFromFile() {
+        Map<String, User> result = new HashMap<String, User>();
+        Scanner scan = new Scanner(Global.class.getResourceAsStream("Users.txt"));
+        while (scan.hasNextLine()) {
+            String nextLine = scan.nextLine();
+            System.out.println(nextLine);
+            String[] split = nextLine.split(" - ");
+            User tempUserp = new User(split[0], Long.parseLong(split[1]));
+            result.put(split[0].toLowerCase(), tempUserp);
+        }
+        return result;
+    }
 }
